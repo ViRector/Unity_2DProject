c2f64e0d9c059d90d4cf3863ef285e7608fa23ec
Spartan-Moon <erqs600@hotmail.com>
2022-02-04 13:08:20 -0600

    Mas carpetas
    - Borré script de mov
    - añadí carpeta de Camara y un script en Collectables

--- Assets/Scripts/Collectables/LifesUI.cs
+++ Assets/Scripts/Collectables/LifesUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mov : MonoBehaviour
+public class LifesUI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
