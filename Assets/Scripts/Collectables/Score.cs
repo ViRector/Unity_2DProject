407686f98573da80c1c5fb9489ab432aef55275c
Spartan-Moon <erqs600@hotmail.com>
2022-02-14 18:23:02 -0600

    Score System
    Added Score System
    Added Game Manager Script

--- Assets/Scripts/Collectables/Score.cs
+++ Assets/Scripts/Collectables/Score.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text scoreText;
+    private float score;
 
     // Update is called once per frame
     void Update()
     {
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            score += 4 * Time.deltaTime;
+            scoreText.text = ((int)score).ToString();
+            //scoreText.text = GameManager.Instance.score.ToString();
+        }
         
+        /*
+         * Switch (killScore)
+         * case Enemy1:
+         * score + 10;
+         * break;
+         * 
+         * case Enemy2:
+         * score + 25;
+         * break;
+         * 
+         * case Enemy3:
+         * score + 50;
+         * break;                          
+        */
     }
 }
